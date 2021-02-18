4054d962f1cdbc5893e3e1c9a3acbf46633e3ea8
kokon322 <chornuy322@gmail.com>
2021-02-18 13:26:02 +0100

    finish

--- lesson1/app.js
+++ lesson1/app.js
@@ -3,51 +3,82 @@ const path = require('path');
 
 const path18_00 = path.join(__dirname, '18_00');
 const path20_00 = path.join(__dirname, '20_00');
+const pathAllStudents = path.join(process.cwd(), 'lesson1', 'allStudents');
 
-fs.readdir(path18_00, ((err, files) => {
+fs.readdir(path18_00, (err, files) => {
     if (err) {
         console.log(err);
         return;
     }
     files.forEach(file => {
-        fs.readFile(path.join(path18_00, file), ((err1, data) => {
+        fs.rename(path.join(path18_00, file), path.join(pathAllStudents, file), (err) => {
             if (err) {
                 console.log(err);
                 return;
             }
-            const user = JSON.parse(data);
-            if (user.gender === 'male') {
-                fs.rename(path.join(path18_00, file), path.join(path20_00, file), (err) => {
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
-                });
-            }
-        }));
+        });
     });
-}));
-
+});
 fs.readdir(path20_00, ((err, files) => {
     if (err) {
         console.log(err);
         return;
     }
     files.forEach(file => {
-        fs.readFile(path.join(path20_00, file), ((err1, data) => {
+        fs.rename(path.join(path20_00, file), path.join(pathAllStudents, file), () => {
             if (err) {
                 console.log(err);
                 return;
             }
-            const user = JSON.parse(data);
-            if (user.gender === 'female') {
-                fs.rename(path.join(path20_00, file), path.join(path18_00, file), (err) => {
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
-                });
-            }
-        }));
+        });
     });
 }));
+
+// fs.readdir(path18_00, ((err, files) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     files.forEach(file => {
+//         fs.readFile(path.join(path18_00, file), ((err1, data) => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             }
+//             const user = JSON.parse(data);
+//             if (user.gender === 'male') {
+//                 fs.rename(path.join(path18_00, file), path.join(path20_00, file), (err) => {
+//                     if (err) {
+//                         console.log(err);
+//                         return;
+//                     }
+//                 });
+//             }
+//         }));
+//     });
+// }));
+//
+// fs.readdir(path20_00, ((err, files) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     files.forEach(file => {
+//         fs.readFile(path.join(path20_00, file), ((err1, data) => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             }
+//             const user = JSON.parse(data);
+//             if (user.gender === 'female') {
+//                 fs.rename(path.join(path20_00, file), path.join(path18_00, file), (err) => {
+//                     if (err) {
+//                         console.log(err);
+//                         return;
+//                     }
+//                 });
+//             }
+//         }));
+//     });
+// }));
+
