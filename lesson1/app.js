3ff8e58f359ca0ee2bc2a052260250785b3f39b3
kokon322 <chornuy322@gmail.com>
2021-02-18 13:02:57 +0100

    finish

---
+++ lesson1/app.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+const path = require('path');
+
+const path18_00 = path.join(__dirname, '18_00');
+const path20_00 = path.join(__dirname, '20_00');
+
+fs.readdir(path18_00, ((err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    files.forEach(file => {
+        fs.readFile(path.join(path18_00, file), ((err1, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            const user = JSON.parse(data);
+            if (user.gender === 'male') {
+                fs.rename(path.join(path18_00, file), path.join(path20_00, file), (err) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                });
+            }
+        }));
+    });
+}));
+
+fs.readdir(path20_00, ((err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    files.forEach(file => {
+        fs.readFile(path.join(path20_00, file), ((err1, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            const user = JSON.parse(data);
+            if (user.gender === 'female') {
+                fs.rename(path.join(path20_00, file), path.join(path18_00, file), (err) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                });
+            }
+        }));
+    });
+}));
+
+
+// fs.readdir(path20_00, ((err, files) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     files.forEach(fileName => {
+//         fs.readFile(path.join(path20_00, fileName), (err, data) => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             }
+//             let user = JSON.parse(data);
+//             if (user.gender === 'male'){
+//                 console.log(user);
+//             }else {
+//                 console.log(`___________`);
+//                 console.log(user);
+//                 console.log(`____________`);
+//             }
+//         })
+//     })
+// }))
+
+// fs.rename(path.join(path18_00, 'Andriy.txt'), path.join(path20_00, `Andriy.txt`), (err) => {
+//     console.log(err);
+// })
