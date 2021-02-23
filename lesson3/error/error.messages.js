bfe922efb62086d199b3b5079293084b308bce01
kokon322 <chornuy322@gmail.com>
2021-02-23 19:20:35 +0100

    some fix

---
+++ lesson3/error/error.messages.js
@@ -0,0 +1,14 @@
+module.exports = {
+    USER_IS_NOT_VALID: {
+        en: 'User is not Valid',
+        ua: 'Юзер не валідний'
+    },
+    ID_IS_NOT_VALID: {
+        en: 'Id is not valid',
+        ua: 'Айді не валідне'
+    },
+    NO_USER_WITH_THIS_ID: {
+        en: 'No user with this id',
+        ua: 'Юзера з таким айді не знайдено'
+    }
+}
--- lesson3/middleware/user.middleware.js
+++ lesson3/middleware/user.middleware.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const {promisify} = require('util');
 const errorCodes = require('../constant/errorCodes.enum');
-const errorMessages = require('../constant/errorMessages.enum');
+const errorMessages = require('../error/error.messages');
 
 const readFile = promisify(fs.readFile);
 
@@ -14,7 +14,7 @@ module.exports = {
         try {
             const userId = +req.params.userId;
             if (userId < 0 || !Number.isInteger(userId) || Number.isNaN(userId)) {
-                throw new Error(errorMessages.idIsNotValid);
+                throw new Error(errorMessages.ID_IS_NOT_VALID['en']);
             }
             next();
         } catch (err) {
@@ -30,7 +30,7 @@ module.exports = {
                 return +user.id === userId;
             });
             if (!result) {
-                throw new Error(errorMessages.notUserWithId);
+                throw new Error(errorMessages.NO_USER_WITH_THIS_ID['en']);
             }
             next();
         } catch (err) {
@@ -47,7 +47,7 @@ module.exports = {
             });
             if (result || name.length < 1 || name.length > 20 || Number.isInteger(name) || password.length < 5 ||
                 email.length < 10 || email.length > 50 || !email.includes('@')) {
-                throw new Error(errorMessages.userIsNotValid);
+                throw new Error(errorMessages.USER_IS_NOT_VALID['en']);
             }
             next();
         } catch (err) {
