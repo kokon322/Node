283a69285f46be08a3b1d1b3609f345c692f1307
kokon322 <chornuy322@gmail.com>
2021-03-01 12:43:10 +0100

    create car middleware

--- lesson4/dataBase/models/Car.js
+++ lesson4/dataBase/models/Car.js
@@ -4,6 +4,6 @@ const carSchema = new Schema({
     producer: { type: String, required: true },
     model: { type: String, required: true },
     price: { type: Number, required: true }
-});
+}, { timestamps: true, toObject: { virtuals: true }, toJSON: { virtuals: true } });
 
 module.exports = model('Car', carSchema);
--- lesson4/dataBase/models/User.js
+++ lesson4/dataBase/models/User.js
@@ -1,15 +1,26 @@
 const { Schema, model } = require('mongoose');
 
-const carSchema = {
-    model: { type: String },
-    price: { type: Number }
-};
-
 const userSchema = new Schema({
     name: { type: String, required: true },
     age: { type: Number, required: true },
     email: { type: String, required: true },
-    cars: [carSchema]
+    cars: [{ type: Schema.Types.ObjectId }]
+}, { timestamps: true, toObject: { virtuals: true }, toJSON: { virtuals: true } });
+
+userSchema.virtual('namePlusAge').get(function() {
+    return `${this.name}  ${this.age}`;
+});
+
+userSchema.virtual('userCars', {
+    ref: 'Car',
+    localField: 'cars',
+    foreignField: '_id'
+});
+
+userSchema.pre('find', function() {
+    this.populate('userCars');
+}).pre('findOne', function() {
+    this.populate('userCars');
 });
 
 module.exports = model('User', userSchema);
